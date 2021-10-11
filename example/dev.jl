d2e559899aad7f6967151a40b66f77b93fd7a866
Picaud Vincent <picaud.vincent@gmail.com>
2021-10-11 19:02:32 +0200

    finir dev.jl et transferer

--- example/dev.jl
+++ example/dev.jl
@@ -102,3 +102,126 @@ restrict_to_inactive!(Q, q, Z, lb, ub)
 (Q,q)
 
 Q2=Rational{Int64}[1//1 0//1 0//1 1//4 1//5 1//6 0//1 0//1 0//1 0//1; 0//1 1//1 0//1 0//1 0//1 0//1 0//1 0//1 0//1 0//1; 0//1 0//1 1//1 0//1 0//1 0//1 0//1 0//1 0//1 0//1; 1//4 0//1 0//1 1//7 1//8 1//9 0//1 0//1 0//1 0//1; 1//5 0//1 0//1 1//8 1//9 1//10 0//1 0//1 0//1 0//1; 1//6 0//1 0//1 1//9 1//10 1//11 0//1 0//1 0//1 0//1; 0//1 0//1 0//1 0//1 0//1 0//1 1//1 0//1 0//1 0//1; 0//1 0//1 0//1 0//1 0//1 0//1 0//1 1//1 0//1 0//1; 0//1 0//1 0//1 0//1 0//1 0//1 0//1 0//1 1//1 0//1; 0//1 0//1 0//1 0//1 0//1 0//1 0//1 0//1 0//1 1//1]
+
+
+""" 
+
+After having modified ``Q, q`` using [``](@ref) one solves
+``x=-Q^{-1}q``. We then compute ``\tau=-(Qx+q)`` to get a posteriori
+mulitpliers.
+
+This function update `Z` according to this inputs. It returns how many
+`Z` components had to be modified. No modification means that the
+multipliers are compatible with the `Z` array, the algorithm has
+converged.
+"""
+function update_Z!(x::AbstractVector,
+                   τ::AbstractVector,
+                   Z::AbstractVector{BoundContraintState_Enum},
+                   lb::AbstractVector,
+                   ub::AbstractVector)
+    n = length(x)
+
+    @assert n == length(τ)
+    @assert n == length(Z)
+    @assert n == length(lb)
+    @assert n == length(ub)
+    
+    count_bad_hypothesis::UInt = 0
+    
+    for i in 1:n
+
+        if Z[i]==BoundContraintState_INACTIVE
+
+            if x[i]<=lb[i]
+
+                count_bad_hypothesis+=1
+                Z[i]=BoundContraintState_LB
+                
+            elseif x[i]>=ub[i]
+                
+                count_bad_hypothesis+=1
+                Z[i]=BoundContraintState_UB
+                
+            end
+            
+        elseif Z[i]==BoundContraintState_LB
+            
+            @assert  x[i]==lb[i] "Internal error $(x[i]) != $(lb[i])"
+            
+            if τ[i]>0
+                count_bad_hypothesis+=1
+                Z[i]=BoundContraintState_INACTIVE
+            end
+
+        else
+            
+          @assert  x[i]==ub[i] "Internal error $(x[i]) != $(ub[i])"
+            
+            if τ[i]<0
+                count_bad_hypothesis+=1
+                Z[i]=BoundContraintState_INACTIVE
+            end
+            
+        end 
+        
+    end #  for i in 1:n
+
+    return count_bad_hypothesis
+end
+
+"""
+Put all together 
+"""
+function Kunisch_Rendl(Q::Symmetric{T},
+                       q::AbtractVector{T},
+                       lb::AbtractVector{T},
+                       ub::AbtractVector{T},
+                       maxIter::UInt = 50,
+                       k0::UInt = 6,
+                       c0::Float64=0.1) where {T<:Real}
+    n = length(q)
+
+    
+    @assert ((n==length(lb))&&
+             (n==length(ub))&&
+             ((n,n)==size(Q))) "Bad dimension"
+
+    @assert count(x->!x,0.<=ub-lb)==0 "Incoherent bounds"
+
+    x=copy(lb)
+    x=min(x,ub)
+    Z=map(x->ifelse(isfinite(x),active_lb,inactive),x)
+
+    for iter in 1:maxIter
+
+        # [Solution]
+        Q_tilde=copy(Q)
+        q_tilde=copy(q)
+
+        if iter<=k0
+            mu::T=c0*norm(Q,Inf)/2^iter
+            for i in 1:n Q_tilde.data[i,i]+=mu end
+        end
+
+        restrict2Active!(Q_tilde,q_tilde,Z,lb,ub)
+
+        x=-1*(Q_tilde\q_tilde);
+        tau=-1*(Q*x+q)
+        # [Solution]
+
+
+        count_bad_hypothesis = updateZ!(x,tau,Z,lb,ub);
+        
+        print("\niter $iter count $count_bad_hypothesis")
+        
+        if (iter>k0)&&(count_bad_hypothesis == 0)
+            print("\nConverged!")
+            return x,tau
+        end
+        
+    end # iter
+    
+    error("\nDid not converged! \n$x")
+    
+end
