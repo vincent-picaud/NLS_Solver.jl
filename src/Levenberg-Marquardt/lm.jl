73f40964103a6b8a269463700b1fb0a1c0e7118d
Picaud Vincent <picaud.vincent@gmail.com>
2021-10-18 18:23:13 +0200

    start lm implementation

--- src/Levenberg-Marquardt/lm.jl
+++ src/Levenberg-Marquardt/lm.jl
@@ -1,6 +1,14 @@
 # Unconstrained problem
 #
+export Levenberg_Marquardt
 
+using LinearAlgebra: norm
+
+# TODO
+# abstract type AbstractNLSSolverConf end
+# struct Levenberg_Marquardt <:  AbstractNLSSolverConf
+# ...
+# end
 
 function Levenberg_Marquardt(nls::AbstractNLS,
                              θ_init::AbstractVector;
@@ -9,13 +17,14 @@ function Levenberg_Marquardt(nls::AbstractNLS,
                              ε_grad_inf_norm::Float64=1e-6,
                              ε_step_2_norm::Float64=1e-6,
                              # initial regularization
-                             τ::Float64=1.0e-3,
-                             ν::Float64=2,
-                             verbose::Bool=true
-                             )
-    # Compute, r,J, ∇fobj
+                             τ::Float64=1.0e-3,                         
+                             ν::Float64=2.0,
+                             verbose::Bool=true)
+    # Compute, r,J, ∇fobj=J'r
     #
+    n_S, n_θ = residue_size(nls),parameter_size(nls) 
     θ=copy(θ_init)
+
     (r,J)=eval_r_J(nls,θ)
     ∇fobj=similar(r)
     eval_nls_∇fobj!(∇fobj,r,J)
@@ -28,5 +37,10 @@ function Levenberg_Marquardt(nls::AbstractNLS,
         return true
     end
 
-    
+    # Compute H=J'J
+    #
+    H=Symmetric(Matrix{eltype(r)}(undef,n_θ,n_θ))
+    eval_nls_∇∇fobj!(H,J)
+
+    true
 end
--- test/Levenberg-Marquardt/lm.jl
+++ test/Levenberg-Marquardt/lm.jl
@@ -6,7 +6,7 @@
         @assert parameter_size(nls) == 2
         @assert residue_size(nls) == 2
 
-	θ=Float64[1,0]
+	θ=Float64[1;0]
 
         (r, J) = eval_r_J(nls,θ)
         
@@ -23,6 +23,15 @@
         eval_nls_∇∇fobj!(H,J)
         @test H ≈ Float64[401.0 -200.0; -200.0 100.0]
     end
-    
+
+    @testset "LM Rosenbrock" begin
+        nls = Rosenbrock()
+        θ=Float64[1;0]
+
+        status=Levenberg_Marquardt(nls, θ)
+
+        @test status
+    end
+        
 end
  
