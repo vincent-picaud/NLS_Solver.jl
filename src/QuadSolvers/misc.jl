9774fb67414ef58cf16d1cb35abf8eb0a1a55bde
Picaud Vincent <picaud.vincent@gmail.com>
2021-10-19 17:25:32 +0200

    doc + slight modification of code

--- src/QuadSolvers/misc.jl
+++ src/QuadSolvers/misc.jl
@@ -4,6 +4,10 @@ import LinearAlgebra: Symmetric
 
 @doc raw"""
 ```julia
+check_first_order(∇f::AbstractVector{<:Real},
+                  xstar::AbstractVector{<:Real},
+                  bc::BoundConstraints{<:Real,1})
+
 check_first_order(Q::Symmetric{<:Real},
                   q::AbstractVector{<:Real},
                   xstar::AbstractVector{<:Real},
@@ -29,18 +33,38 @@ This is equivalent to:
 x^\star = P_{[l,u]}(x^\star-\nabla f(x^\star))
 ```
 
-Using the previous results, this function returns:
+According to the previous result, this function returns:
 ```math
 \max \mid x^\star - P_{[l,u]}(x^\star-(Q.x^\star+q)) \mid
 ```
-for a local optimal quantity this must be null 
+
+For a local stationary point this quantity must be null 
+
+The second function is a wrapper that computes ``∇f=Q.x^\star+q``
 """
+function check_first_order(∇f::AbstractVector{<:Real},
+                           xstar::AbstractVector{<:Real},
+                           bc::BoundConstraints{<:Real,1})
+
+    # the condition assumes feasible constraints
+    @assert xstar ∈ bc
+
+    @assert size(∇f)==size(xstar)
+
+    # TODO: refactoring to avoid mem alloc
+    # (-> inline computation of max(...sequence...)
+    v = xstar-∇f
+    v = project!(v,bc)
+
+    maximum(abs.(xstar .- v))
+end
+
 function check_first_order(Q::Symmetric{<:Real},
                            q::AbstractVector{<:Real},
                            xstar::AbstractVector{<:Real},
                            bc::BoundConstraints{<:Real,1})
-    v = xstar-(Q*xstar+q)
-    v = project!(v,bc)
-
-    maximum(abs.(xstar .- v))
+    # TODO: for BlasFloat use BLAS... to avoid unnecessary memory
+    # allocs...
+    ∇f=Q*xstar+q
+    check_first_order(∇f,xstar,bc)
 end 
