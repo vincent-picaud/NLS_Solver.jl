4b5a5f127cbc765ce266238a0eafe7f7dc49a636
Picaud Vincent <picaud.vincent@gmail.com>
2021-10-21 13:14:49 +0200

    remove unused stuff

--- test/QuadSolvers/misc.jl
+++ test/QuadSolvers/misc.jl
@@ -1,18 +1,5 @@
 
 @testset "Q+μD" begin
-    using LinearAlgebra: Symmetric, I
-    using NLS_Solver: compute_Q_μD
-    
-    n = 5
-    
-    ref = 2.0*I(n)
-
-    Q=Symmetric(zeros(n,n))
-
-    Q1 = compute_Q_μD(Q,2.0,I)
-    @test Q1 ≈ ref
-
-    Q2 = compute_Q_μD(Q,2.0,ones(n))
-    @test Q2 ≈ ref
+    # This was removed (not used)
 end
  
